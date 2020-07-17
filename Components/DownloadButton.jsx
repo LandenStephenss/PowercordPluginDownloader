d3984c6e6bfb2e6d996e785c82fc60147d27833d
LandenStephenss <turtlederp806@icloud.com>
2020-07-17 13:09:52 -0500

    alright. should fix the crashing

--- Components/DownloadButton.jsx
+++ Components/DownloadButton.jsx
@@ -7,31 +7,50 @@ const DownloadPlugin = require("../downloadPlugin");
 const downloadPlugin = require("../downloadPlugin");
 class DownloadButton extends React.Component {
   render() {
-    var GithubLink = this.props.message.content
-      .replace(/(?:\n|<|>)/g, " ")
-      .split(" ")
-      .filter((f) =>
-        f.match(/^https?:\/\/(www.)?git(hub|lab).com\/[\w-]+\/[\w-]+/)
-      )[0];
-    console.log(GithubLink);
-    const repoName = GithubLink.match(/[\w-]+$/)[0];
-    var installed = powercord.pluginManager.isInstalled(repoName);
-    return (
-      <div
-        className={["PluginDownloaderApply", true && "applied"]
-          .filter(Boolean)
-          .join(" ")}
-      >
-        <Clickable
-          onClick={() => {
-            if (installed) return;
-            downloadPlugin(GithubLink, powercord);
-          }}
+    if (!this.props.message.content.includes("https://github.com")) {
+      return (
+        <div
+          className={["PluginDownloaderApply", true && "applied"]
+            .filter(Boolean)
+            .join(" ")}
         >
-          {installed ? "Plugin Installed" : "Download Plugin"}
-        </Clickable>
-      </div>
-    );
+          <Clickable
+            onClick={() => {
+              if (installed) return;
+              downloadPlugin(GithubLink, powercord);
+            }}
+          >
+            No Plugin
+          </Clickable>
+        </div>
+      );
+    } else {
+      var GithubLink = this.props.message.content
+        .replace(/(?:\n|<|>)/g, " ")
+        .split(" ")
+        .filter((f) =>
+          f.match(/^https?:\/\/(www.)?git(hub|lab).com\/[\w-]+\/[\w-]+/)
+        )[0];
+      console.log(GithubLink);
+      const repoName = GithubLink.match(/[\w-]+$/)[0];
+      var installed = powercord.pluginManager.isInstalled(repoName);
+      return (
+        <div
+          className={["PluginDownloaderApply", true && "applied"]
+            .filter(Boolean)
+            .join(" ")}
+        >
+          <Clickable
+            onClick={() => {
+              if (installed) return;
+              downloadPlugin(GithubLink, powercord);
+            }}
+          >
+            {installed ? "Plugin Installed" : "Download Plugin"}
+          </Clickable>
+        </div>
+      );
+    }
   }
 }
 
