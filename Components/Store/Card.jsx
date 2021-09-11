25a4335cf3be0d3ac8aa72263975bc76f2abe203
Loremly <LoremlyDev@gmail.com>
2021-09-10 20:10:33 -0400

    Added styling and some functions
    Just added to card.jsx and styled it.

--- Components/Store/Card.jsx
+++ Components/Store/Card.jsx
@@ -26,9 +26,18 @@ module.exports = class Card extends React.Component {
         } else {
             return (
                 <div className="PPD-StoreCard">
-                    <Clickable onClick={() => {
-                        openExternal(GithubLink)
-                    }} className="PPD-CardTitle"><strong>{repoName}</strong></Clickable>
+                    <h1 className="PPD-CardTitle">
+                        <span>{repoName}</span>
+                        <Tooltip position="top" text="Go to repository">
+                            <Clickable className="redirect" onClick={() => { openExternal(GithubLink)}}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
+                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
+                                    <polyline points="15 3 21 3 21 9"/>
+                                    <line x1="10" y1="14" x2="21" y2="3"/>
+                                </svg>
+                            </Clickable>
+                        </Tooltip>
+                    </h1>
 
                     <div className="PPD-Details">
                         <div className="PPD-CardContent">
@@ -36,7 +45,7 @@ module.exports = class Card extends React.Component {
                                 <Tooltip position="top" text="Description">
                                     <Receipt height={24} width={24} />
                                 </Tooltip>
-                                <span>Test</span>
+                                <span>This is a description test of a plugin</span>
                             </div>
 
                             <div className="PPD-Metadata">
@@ -47,8 +56,10 @@ module.exports = class Card extends React.Component {
                                     <span>{this.message.author.nick}</span>
                                 </div>
                                 <div className="PPD-Version">
-                                    <Tag width={24} height={24} />
-                                    <span> v1</span>
+                                    <Tooltip position="top" text="Version">
+                                        <Tag width={24} height={24} />
+                                    </Tooltip>
+                                    <span> Version </span>
                                 </div>
                             </div>
                         </div>
