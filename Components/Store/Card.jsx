71f6a85a99ac9927fa40dab3b560065bc620c8e9
akisblack <mavrommatisakis@gmail.com>
2021-09-13 19:30:54 +0300

    use classes from powercord

--- Components/Store/Card.jsx
+++ Components/Store/Card.jsx
@@ -23,39 +23,39 @@ module.exports = class Card extends React.Component {
             return null
         } else {
             return (
-                <div className="PPD-StoreCard">
-                    <h1 className="PPD-CardTitle">
-                        <span>{repoName}</span>
-                        <Tooltip position="top" text="Go to repository">
-                            <Clickable className="redirect" onClick={() => { openExternal(GithubLink)}}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
-                                    <polyline points="15 3 21 3 21 9"/>
-                                    <line x1="10" y1="14" x2="21" y2="3"/>
-                                </svg>
-                            </Clickable>
-                        </Tooltip>
-                        <Button disabled={this.message.message.embeds.length <= 0 && this.message.message.attachments.length <= 0} onClick={() => openModal(() => React.createElement(Preview, {message: this.message.message}))}>Open preview</Button>
-                    </h1>
-
-                    <div className="PPD-Details">
-                        <div className="PPD-CardContent">
-                            <div className="PPD-CardDescription">
+                <div className="PPD-Card">
+                    <div className="powercord-product cardPrimary-1Hv-to card-3Qj_Yx">
+                        <h4 className="powercord-product-header">
+                            <span>{repoName}</span>
+                            <Tooltip position="top" text="Go to repository">
+                                <Clickable className="redirect" onClick={() => { openExternal(GithubLink)}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
+                                        <polyline points="15 3 21 3 21 9"/>
+                                        <line x1="10" y1="14" x2="21" y2="3"/>
+                                    </svg>
+                                </Clickable>
+                            </Tooltip>
+                            <Button disabled={this.message.message.embeds.length <= 0 && this.message.message.attachments.length <= 0} onClick={() => openModal(() => React.createElement(Preview, {message: this.message.message}))}>Open preview</Button>
+                        </h4>
+                        <div className="divider-3573oO dividerDefault-3rvLe-"></div>
+                        <div className="powercord-product-details">
+                            <div className="description">
                                 <Tooltip position="top" text="Description">
                                     <Receipt height={24} width={24} />
                                 </Tooltip>
                                 <span>This is a description test of a plugin</span>
                             </div>
-
-                            <div className="PPD-Metadata">
-                                <div className="PPD-Author">
+    
+                            <div className="metadata">
+                                <div className="author">
                                     <Tooltip position="top" text="Author">
                                         <Person width={24} height={24} />
                                     </Tooltip>
                                     <span>{this.message.author?.nick || this.message.message.author.username}</span> 
-
+    
                                 </div>
-                                <div className="PPD-Version">
+                                <div className="version">
                                     <Tooltip position="top" text="Version">
                                         <Tag width={24} height={24} />
                                     </Tooltip>
@@ -63,22 +63,24 @@ module.exports = class Card extends React.Component {
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-
-                    <div className="PPD-StoreCardFooter">
-                        <div className="PPD-StoreButtons">
-                            {this.state.installed ?
-                                <Button disabled color={Button.Colors.RED}>Already Installed</Button> :
-                                <Button onClick={() => {
-                                    if (this.state.installed) return;
-                                    DownloadPlugin(GithubLink, powercord);
-                                    this.setState({installed: true});
-                                }}>Install</Button>
-                            }
+    
+                        <div className="divider-3573oO dividerDefault-3rvLe-"></div>
+    
+    
+                        <div className="powercord-product-footer">
+                            <div className="buttons">
+                                {this.state.installed ?
+                                    <Button disabled color={Button.Colors.RED}>Already Installed</Button> :
+                                    <Button onClick={() => {
+                                        if (this.state.installed) return;
+                                        DownloadPlugin(GithubLink, powercord);
+                                        this.setState({installed: true});
+                                    }}>Install</Button>
+                                }
+                            </div>
                         </div>
+    
                     </div>
-
                 </div>
             )
         }
